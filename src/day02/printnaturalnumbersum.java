7be4bae3181098fd0afa66dbf922838c02223143
Sneha Harurkar <harurkar.6719@gmail.com>
2026-02-04 21:39:06 +0530

    Day 2: loops practice – print first N natural numbers

---
+++ src/day02/printnaturalnumbersum.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class printnaturalnumbersum {
+    static void main() {
+        int n=4;
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum=sum+i;
+
+        }
+        System.out.println(sum);
+
+    }
+}
