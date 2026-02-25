9a6b80131e6a0f055ba381442f59f3bc4188baab
Sneha Harurkar <harurkar.6719@gmail.com>
2026-02-25 20:46:56 +0530

    Day 23: rest api

---
+++ src/day23/RestController.java
@@ -0,0 +1,28 @@
+package day23;
+
+
+    import org.springframework.web.bind.annotation.*;
+    import java.util.ArrayList;
+    import java.util.List;
+    public @interface RestController {
+
+    @RestController
+    @RequestMapping("/students")
+    public class StudentController {
+
+        List<String> students = new ArrayList<>();
+
+        // GET - fetch all students
+        @GetMapping
+        public List<String> getStudents() {
+            return students;
+        }
+
+        // POST - add new student
+        @PostMapping
+        public String addStudent(@RequestBody String name) {
+            students.add(name);
+            return "Student added successfully!";
+        }
+    }
+}
