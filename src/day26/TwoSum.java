fc38847ea9f5c4b70429f82763f80735897f071f
Sneha Harurkar <harurkar.6719@gmail.com>
2026-02-28 20:12:13 +0530

    Day 26: two sum

---
+++ src/day26/TwoSum.java
@@ -0,0 +1,33 @@
+package day26;
+
+import java.util.*;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();   // size of array
+        int arr[] = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+
+                if(arr[i] + arr[j] == target){
+                    System.out.println(i + " " + j);
+                    return;
+                }
+            }
+        }
+
+        System.out.println("No solution");
+    }
+}
