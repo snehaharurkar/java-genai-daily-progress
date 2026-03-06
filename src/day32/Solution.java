89504354fb2cc12f704392eb7456839a0e4e6e04
Sneha Harurkar <harurkar.6719@gmail.com>
2026-03-06 21:56:06 +0530

    Day 32: dsa

---
+++ src/day32/Solution.java
@@ -0,0 +1,24 @@
+package day32;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int breadth = sc.nextInt();
+        int height = sc.nextInt();
+
+        if (breadth <= 0 || height <= 0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+        else {
+            int area = breadth * height;
+            System.out.println(area);
+        }
+
+        sc.close();
+    }
+}
